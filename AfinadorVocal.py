35298bf0791fb5268fe8cafc7a1ba747eccd0d77
AllanDeuz <allan_1_20@hotmail.com>
2022-05-22 01:02:15 -0400

    Se inicio proyecto, se instalaron librerias+

---
+++ AfinadorVocal.py
@@ -0,0 +1,9 @@
+import numpy as np
+import scipy.fftpack as fourier
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy.io.wavfile as waves
+import pyaudio as pa 
+import struct 
+import statsmodels.api as sm
+from scipy.signal import find_peaks

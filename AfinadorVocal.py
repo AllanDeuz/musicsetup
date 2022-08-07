e1acef1b5e04d46e597a2c73aa1d295504db16ff
AllanDeuz <allan_1_20@hotmail.com>
2022-08-07 09:10:01 -0400

    refactorizacion implementada

--- AfinadorVocal.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import scipy.fftpack as fourier
-import matplotlib
-import matplotlib.pyplot as plt
-import scipy.io.wavfile as waves
-import pyaudio as pa 
-import struct 
-import statsmodels.api as sm
-from scipy.signal import find_peaks

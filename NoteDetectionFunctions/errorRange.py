f9d8ffe9795480861c243441a15867d5d84f22ff
AllanDeuz <allan_1_20@hotmail.com>
2022-08-30 16:01:08 -0400

    Codigo final completo

--- NoteDetectionFunctions/errorRange.py
+++
@@ -1,4 +0,0 @@
-def errorRange (difference):
-    if difference  >= 0 and difference < 5:
-        return(True)
-    else: return(False)
--- NoteDetectionFunctions/hzApproximation.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def approximateHz (hzType, maxHz):
-    hzApproximation = hzType.flat[np.abs(hzType - maxHz).argmin()]
-    hzApproximation = np.round(hzApproximation, 3)
-    return hzApproximation
